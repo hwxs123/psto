bf837ea5d9bc0983b895d498143c25da84ab5ff0
hwxs123 <72619803+hwxs123@users.noreply.github.com>
2025-02-04 12:03:51 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '';//随便备注
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLmZ4eGsuZGVkeW4uaW8=');
